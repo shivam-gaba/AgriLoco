ef310f925b481ea92557035a24b133daf02da5aa
Shivam gaba <shivamgabagaba@gmail.com>
2020-07-22 19:47:18 +0530

    Edit App confirmation on FarmerDashboard

--- lib/Screens/Dashboards/FarmerDashboard.dart
+++ lib/Screens/Dashboards/FarmerDashboard.dart
@@ -2,7 +2,9 @@ import 'package:agri_loco/Components/CustomFieldTile.dart';
 import 'package:agri_loco/Models/LoginData.dart';
 import 'package:agri_loco/Screens/InputSheets/FieldDetailsInputSheet.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cool_alert/cool_alert.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:provider/provider.dart';
@@ -36,76 +38,92 @@ class _FarmerDashboardState extends State<FarmerDashboard> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<LoginData>(
-        builder: (BuildContext context, LoginData loginData, Widget child) {
-      return ModalProgressHUD(
-        inAsyncCall: _isSpinnerShowing,
-        child: MaterialApp(
-            home: Scaffold(
-          appBar: AppBar(
-            leading: Icon(
-              Icons.filter_hdr,
+    return WillPopScope(
+      onWillPop: () {
+        CoolAlert.show(
+            context: context,
+            type: CoolAlertType.confirm,
+            confirmBtnColor: Colors.green.shade900,
+            title: 'Do you want to Exit ?',
+            confirmBtnText: 'Yes',
+            showCancelBtn: true,
+            cancelBtnText: 'No',
+            onConfirmBtnTap: () {
+              return SystemNavigator.pop();
+            });
+        return;
+      },
+      child: Consumer<LoginData>(
+          builder: (BuildContext context, LoginData loginData, Widget child) {
+        return ModalProgressHUD(
+          inAsyncCall: _isSpinnerShowing,
+          child: MaterialApp(
+              home: Scaffold(
+            appBar: AppBar(
+              leading: Icon(
+                Icons.filter_hdr,
+              ),
+              backgroundColor: Colors.green.shade900,
+              title: Text('AGRI LOCO',
+                  style: GoogleFonts.indieFlower(
+                    letterSpacing: 3,
+                    fontWeight: FontWeight.bold,
+                  )),
             ),
-            backgroundColor: Colors.green.shade900,
-            title: Text('AGRI LOCO',
-                style: GoogleFonts.indieFlower(
-                  letterSpacing: 3,
-                  fontWeight: FontWeight.bold,
-                )),
-          ),
-          backgroundColor: Colors.greenAccent,
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.green.shade900,
-            onPressed: () {
-              showModalBottomSheet(
-                  context: context,
-                  builder: (context) {
-                    return FieldDetailsInputSheet();
-                  });
-            },
-            child: Icon(Icons.add),
-          ),
-          body: StreamBuilder<QuerySnapshot>(
-            stream: _fireStore
-                .collection('FieldsData')
-                .document(loginData.adhaarNumber)
-                .collection('FarmerFieldData')
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Center(
-                  child: CircularProgressIndicator(),
+            backgroundColor: Colors.greenAccent,
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.green.shade900,
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return FieldDetailsInputSheet();
+                    });
+              },
+              child: Icon(Icons.add),
+            ),
+            body: StreamBuilder<QuerySnapshot>(
+              stream: _fireStore
+                  .collection('FieldsData')
+                  .document(loginData.adhaarNumber)
+                  .collection('FarmerFieldData')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                var fields = snapshot.data.documents;
+                List<CustomFieldTile> fieldsList = [];
+
+                for (var field in fields) {
+                  fieldsList.add(CustomFieldTile(
+                      onEditClicked: () {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (context) {
+                              return FieldDetailsInputSheet();
+                            });
+                      },
+                      onDeleteClicked: () {
+                        deleteField(loginData, field);
+                      },
+                      khasraNumber: field.documentID,
+                      cropType: field.data['cropType'],
+                      fieldSize: field.data['fieldSize'],
+                      waterSource: field.data['waterSource']));
+                }
+
+                return ListView(
+                  children: fieldsList,
                 );
-              }
-
-              var fields = snapshot.data.documents;
-              List<CustomFieldTile> fieldsList = [];
-
-              for (var field in fields) {
-                fieldsList.add(CustomFieldTile(
-                    onEditClicked: () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (context) {
-                            return FieldDetailsInputSheet();
-                          });
-                    },
-                    onDeleteClicked: () {
-                      deleteField(loginData, field);
-                    },
-                    khasraNumber: field.documentID,
-                    cropType: field.data['cropType'],
-                    fieldSize: field.data['fieldSize'],
-                    waterSource: field.data['waterSource']));
-              }
-
-              return ListView(
-                children: fieldsList,
-              );
-            },
-          ),
-        )),
-      );
-    });
+              },
+            ),
+          )),
+        );
+      }),
+    );
   }
 }
